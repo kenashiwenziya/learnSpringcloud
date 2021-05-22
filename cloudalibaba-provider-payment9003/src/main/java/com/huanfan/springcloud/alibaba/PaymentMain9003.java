bb7cd818c6f42198da072fffb478b82aaa9acde3
wenfan.liu <2551468282@qq.com>
2021-05-23 00:08:39 +0800

    learning...

---
+++ cloudalibaba-provider-payment9003/src/main/java/com/huanfan/springcloud/alibaba/PaymentMain9003.java
@@ -0,0 +1,18 @@
+package com.huanfan.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @auther lwf
+ * @create 2021-05-22
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9003
+{
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9003.class, args);
+    }
+}
